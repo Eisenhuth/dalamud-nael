eeffe4810596a873f7eb86ba5856a125cb6d3539
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2023-04-06 22:38:33 +0200

    added support for French, German and Japanese (#2)
    * bump version number
    
    * added support for French, German and Japanese
    
    - quotes are now read from the embedded json.
    - test now prints all the quotes in chat
    - quotes now use the client language
    
    * Update README.md
    
    * Update NaelPlugin.json

---
+++ nael/nael/NaelQuotes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace nael;
+
+public struct NaelQuotes
+{
+    public List<Quote> Quotes { get; set; }
+}
+
+public struct Quote
+{
+    public int ID { get; set; }
+    public Text Text { get; set; }
+}
+
+public struct Text
+{
+    public string Text_de { get; set; }
+    public string Text_en { get; set; }
+    public string Text_fr { get; set; }
+    public string Text_ja { get; set; }
+}
